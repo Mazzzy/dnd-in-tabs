6b67a99863b39f495175e48cf87e49c0e9cc0183
Mazahar Shaikh <mazahar.shaikh@bfhl.in>
2020-03-03 13:36:25 +0530

    add drop and update tab count feature

--- src/components/tabs/Tab.js
+++ src/components/tabs/Tab.js
@@ -1,17 +1,33 @@
 import React from 'react';
+import { useDrop } from 'react-dnd';
 
-function Tab({ tab, onClick, activeTab }) {
-    return (
-      <li
-        key={tab.name}
-        className={`tab-list-item ${
-          activeTab?.name === tab.name ? "tab-list-active" : ""
-        }`}
-        onClick={onClick}
-      >
-        {tab.name} ({tab.count})
-      </li>
-    );
+function Tab({ tab, onClick, activeTab, updateTabCounts, setDroppedTab, droppedTab }) {
+  const [, drop] = useDrop({
+    accept: 'CARD',
+    id: 123,
+    drop(item, monitor) {
+      updateTabCounts(activeTab.name, tab.name);
+      setDroppedTab(tab.name);
+      setTimeout(() => setDroppedTab(), 300);
+      return { tab };
+    }
+  });
+
+  return (
+    <li
+      ref={drop}
+      key={tab.name}
+      className={`tab-list-item ${
+        activeTab?.name === tab.name ? 'tab-list-active' : ''
+       } ${
+        droppedTab === tab.name ? 'tab-item-dropped' : ''
+       }
+      `}
+      onClick={onClick}
+    >
+      {tab.name} ({tab.count})
+    </li>
+  );
 }
 
 export default Tab;
